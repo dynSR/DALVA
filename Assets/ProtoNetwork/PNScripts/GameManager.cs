0965dc8afe863cb62ace791cc104d25588f9728e
GERARD_Maxime <maxime.gerard@e-artsup.net>
2020-10-26 16:18:13 +0100

    Part 1 Photon + build settings

---
+++ Assets/ProtoNetwork/PNScripts/GameManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+using Photon.Pun;
+using Photon.Realtime;
+
+namespace ProtoNetwork
+{
+    public class GameManager : MonoBehaviourPunCallbacks
+    {
+        #region Photon Callbacks
+
+        public override void OnLeftRoom()
+        {
+            SceneManager.LoadScene("PNLauncher");
+        }
+
+        #endregion
+
+        #region Public Methods
+
+        public void LeaveRoom()
+        {
+            PhotonNetwork.LeaveRoom();
+        }
+
+        #endregion
+    }
+}
+
---
+++ Assets/ProtoNetwork/PNScripts/PNPlayerNameInputField.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+using TMPro;
+
+using Photon.Pun;
+using Photon.Realtime;
+
+namespace ProtoNetwork
+{
+    // Player name input field. Let the user input his name, will appear above the player in the game.
+    [RequireComponent(typeof(TMP_InputField))]
+
+    public class PNPlayerNameInputField : MonoBehaviour
+    {
+        #region Private Constants
+
+        // Store the PlayerPref Key to avoid typos
+        const string playerNamePrefKey = "PlayerName";
+
+        #endregion
+
+        #region MonoBehaviour CallBacks
+
+        void Start()
+        {
+            string defaultName = string.Empty;
+            TMP_InputField _inputField = this.GetComponent<TMP_InputField>();
+            if (_inputField != null)
+            {
+                if (PlayerPrefs.HasKey(playerNamePrefKey))
+                {
+                    defaultName = PlayerPrefs.GetString(playerNamePrefKey);
+                    _inputField.text = defaultName;
+                }
+            }
+            PhotonNetwork.NickName = defaultName;
+        }
+
+        #endregion
+
+        #region Public Methods
+
+        public void SetPlayerName(string value)
+        {
+            // #Important
+            if (string.IsNullOrEmpty(value))
+            {
+                Debug.LogError("Player Name is null or empty");
+                return;
+            }
+            PhotonNetwork.NickName = value;
+
+
+            PlayerPrefs.SetString(playerNamePrefKey, value);
+        }
+
+        #endregion
+    }
+}

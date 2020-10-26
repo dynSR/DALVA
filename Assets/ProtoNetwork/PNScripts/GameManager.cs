4ac0366e59fe9ee6813bbfaf0e129773a0acbc0d
GERARD_Maxime <maxime.gerard@e-artsup.net>
2020-10-26 16:40:38 +0100

    Part 1 Photon finished

--- Assets/ProtoNetwork/PNScripts/GameManager.cs
+++ Assets/ProtoNetwork/PNScripts/GameManager.cs
@@ -17,6 +17,32 @@ namespace ProtoNetwork
             SceneManager.LoadScene("PNLauncher");
         }
 
+        public override void OnPlayerEnteredRoom(Player other)
+        {
+            Debug.LogFormat("OnPlayerEnteredRoom() {0}", other.NickName); // not seen if you're the player connecting
+
+            if (PhotonNetwork.IsMasterClient)
+            {
+                Debug.LogFormat("OnPlayerEnteredRoom IsMasterClient {0}", PhotonNetwork.IsMasterClient); // called before OnPlayerLeftRoom
+
+                LoadArena();
+            }
+        }
+
+        public override void OnPlayerLeftRoom(Player other)
+        {
+            Debug.LogFormat("OnPlayerLeftRoom() {0}", other.NickName); // seen when other disconnects
+
+
+            if (PhotonNetwork.IsMasterClient)
+            {
+                Debug.LogFormat("OnPlayerLeftRoom IsMasterClient {0}", PhotonNetwork.IsMasterClient); // called before OnPlayerLeftRoom
+
+
+                LoadArena();
+            }
+        }
+
         #endregion
 
         #region Public Methods
@@ -24,6 +50,20 @@ namespace ProtoNetwork
         public void LeaveRoom()
         {
             PhotonNetwork.LeaveRoom();
+        }
+
+        #endregion
+
+        #region Private Methods
+
+        void LoadArena()
+        {
+            if (!PhotonNetwork.IsMasterClient)
+            {
+                Debug.LogError("PhotonNetwork : Trying to Load a level but we are not the master Client");
+            }
+            Debug.LogFormat("PhotonNetwork : Loading Level : {0}", PhotonNetwork.CurrentRoom.PlayerCount);
+            PhotonNetwork.LoadLevel("PNSceneFor" + PhotonNetwork.CurrentRoom.PlayerCount);
         }
 
         #endregion
--- Assets/ProtoNetwork/PNScripts/PNLauncher.cs
+++ Assets/ProtoNetwork/PNScripts/PNLauncher.cs
@@ -23,6 +23,8 @@ namespace ProtoNetwork
         // This client's version number. Users are separated from each other by gameVersion (which allows you to make breaking changes).
         string gameVersion = "1";
 
+        bool isConnecting;
+
         #endregion
 
         #region Public Fields
@@ -57,9 +59,12 @@ namespace ProtoNetwork
 
         public override void OnConnectedToMaster()
         {
-            // #Critical: The first we try to do is to join a potential existing room. If there is, good, else, we'll be called back with OnJoinRandomFailed()
-            PhotonNetwork.JoinRandomRoom();
-            Debug.Log("PUN Basics Tutorial/Launcher: OnConnectedToMaster() was called by PUN");
+            if (isConnecting)
+            {
+                // #Critical: The first we try to do is to join a potential existing room. If there is, good, else, we'll be called back with OnJoinRandomFailed()
+                PhotonNetwork.JoinRandomRoom();
+                isConnecting = false;
+            }
         }
 
         public override void OnDisconnected(DisconnectCause cause)
@@ -68,6 +73,7 @@ namespace ProtoNetwork
             controlPanel.SetActive(true);
 
             Debug.LogWarningFormat("PUN Basics Tutorial/Launcher: OnDisconnected() was called by PUN with reason {0}", cause);
+            isConnecting = false;
         }
 
         public override void OnJoinRandomFailed(short returnCode, string message)
@@ -81,6 +87,16 @@ namespace ProtoNetwork
         public override void OnJoinedRoom()
         {
             Debug.Log("PUN Basics Tutorial/Launcher: OnJoinedRoom() called by PUN. Now this client is in a room.");
+
+            // #Critical: We only load if we are the first player, else we rely on `PhotonNetwork.AutomaticallySyncScene` to sync our instance scene.
+            if (PhotonNetwork.CurrentRoom.PlayerCount == 1)
+            {
+                Debug.Log("We load the 'Room for 1' ");
+
+                // #Critical
+                // Load the Room Level.
+                PhotonNetwork.LoadLevel("PNSceneFor1");
+            }
         }
 
         #endregion
@@ -97,16 +113,13 @@ namespace ProtoNetwork
             connectLabel.SetActive(true);
             controlPanel.SetActive(false);
 
-            // we check if we are connected or not, we join if we are , else we initiate the connection to the server.
             if (PhotonNetwork.IsConnected)
             {
-                // #Critical we need at this point to attempt joining a Random Room. If it fails, we'll get notified in OnJoinRandomFailed() and we'll create one.
                 PhotonNetwork.JoinRandomRoom();
             }
             else
             {
-                // #Critical, we must first and foremost connect to Photon Online Server.
-                PhotonNetwork.ConnectUsingSettings();
+                isConnecting = PhotonNetwork.ConnectUsingSettings();
                 PhotonNetwork.GameVersion = gameVersion;
             }
         }

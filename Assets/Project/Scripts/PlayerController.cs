26b3435b8dee74a7546a6d7b489871ed4c15d542
Dylan Schumacker <45655723+daSPR1NG@users.noreply.github.com>
2020-10-26 14:07:08 +0100

    *.*

---
+++ Assets/Project/Scripts/PlayerController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}

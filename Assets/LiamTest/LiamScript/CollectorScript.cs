907ce24f07249867a27c87be69151f86177d67f0
Liam <liam.rogron@e-artsup.net>
2020-10-28 17:37:44 +0100

    added new scene and new scripts for testing

---
+++ Assets/LiamTest/LiamScript/CollectorScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+    public class CollectorScript : MonoBehaviour
+    {
+
+        public int tier1 = 0;
+        public int maxTier1 = 250;
+
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+
+        public void StartCollect()
+        {
+
+        }
+
+    }

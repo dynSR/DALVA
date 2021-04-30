77ce1c8093f0eec9594c0ed49ffd9ffd9d3461bd
GERARD_Maxime <maxime.gerard@e-artsup.net>
2021-04-30 18:18:15 +0200

    Difficulty button logic finished + Added the cute little gradient border <3 + added the "all" in MalusType (makes the life easier)

---
+++ Assets/Project/Code/Scripts/UI/MainMenu/DifficultyButton.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DifficultyButton : MonoBehaviour
+{
+    public MalusType malus;
+    public bool plus;
+}
---
+++ Assets/Project/Code/Scripts/UI/MainMenu/MainMenuUIManager.cs
@@ -0,0 +1,265 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public enum MalusType {Life = 0, Attack = 1, Speed = 2, Stele = 3, All = 4}
+
+public class MainMenuUIManager : MonoBehaviour
+{
+    #region Assets
+
+    //General assets
+    [Header("General assets")]
+    [Tooltip("The big black image for the fade in/fade out transition.")]
+    public Image fadeInImage;
+    [Tooltip("The name of the game scene.")]
+    public string gameSceneName;
+
+    //Main tab's buttons (always visible)
+    [Header("Main tab")]
+    [Tooltip("The play button from the main tab (always visible).")]
+    public Button playTabButton;
+    [Tooltip("The encyclopedia button from the main tab (always visible).")]
+    public Button encyclopediaTabButton;
+    [Tooltip("The shop button from the main tab (always visible).")]
+    public Button shopTabButton;
+    [Tooltip("The options button from the main tab (always visible).")]
+    public Button optionsTabButton;
+    [Tooltip("The quit button from the main tab (always visible).")]
+    public Button quitTabButton;
+
+    //Secondary tabs (not always visible)
+    [Header("Secondary tabs")]
+    [Tooltip("The play tab.")]
+    public GameObject playTab;
+    [Tooltip("The encyclopedia tab.")]
+    public GameObject encyclopediaTab;
+    [Tooltip("The shop tab.")]
+    public GameObject shopTab;
+    [Tooltip("The options tab.")]
+    public GameObject optionsTab;
+    [Tooltip("The quit tab.")]
+    public GameObject quitTab;
+
+    //Play Tab
+    [Header("Play tab assets")]
+        //Class
+    [Tooltip("The Warrior class button.")]
+    public Button warriorButton;
+    [Tooltip("The Mage class button.")]
+    public Button mageButton;
+    [Space(10)]
+        //Difficulty
+            //Life assets
+    [Tooltip("The minus button for the enemies' life bonus.")]
+    public Button minusLifeButton;
+    [Tooltip("The plus button for the enemies' life bonus.")]
+    public Button plusLifeButton;
+    [Tooltip("The text of the current value of the enemies' life bonus.")]
+    public TMP_Text currentLifeMalusText;
+    [Space(10)]
+            //Attack assets
+    [Tooltip("The minus button for the enemies' attack bonus.")]
+    public Button minusAttackButton;
+    [Tooltip("The plus button for the enemies' attack bonus.")]
+    public Button plusAttackButton;
+    [Tooltip("The text of the current value of the enemies' attack bonus.")]
+    public TMP_Text currentAttackMalusText;
+    [Space(10)]
+            //Speed assets
+    [Tooltip("The minus button for the enemies' speed bonus.")]
+    public Button minusSpeedButton;
+    [Tooltip("The plus button for the enemies' speed bonus.")]
+    public Button plusSpeedButton;
+    [Tooltip("The text of the current value of the enemies' speed bonus.")]
+    public TMP_Text currentSpeedMalusText;
+    [Space(10)]
+            //Stele assets
+    [Tooltip("The minus button for the stele malus.")]
+    public Button minusSteleButton;
+    [Tooltip("The plus button for the stele malus.")]
+    public Button plusSteleButton;
+    [Tooltip("The text of the current value of the stele malus.")]
+    public TMP_Text currentSteleMalusText;
+    [Space(10)]
+            //Difficulty text
+    [Tooltip("The text  for the easy difficulty.")]
+    public GameObject easyText;
+    [Tooltip("The text  for the medium difficulty.")]
+    public GameObject mediumText;
+    [Tooltip("The text  for the hard difficulty.")]
+    public GameObject hardText;
+    [Tooltip("The text  for the impossible difficulty.")]
+    public GameObject impossibleText;
+
+    #endregion
+
+    #region Fields
+
+    //General
+    private GameObject currentOpenedTab;
+    private GameObject nextOpenedTab;
+
+    private bool characterClass; //false = Warrior, true = mage
+
+    //Difficulty
+    private int totalDifficulty;
+    private int mediumDifficulty;
+    private int hardDifficulty;
+    private int impossibleDifficulty;
+
+    [Header("Difficulty meter values")]
+    public int maxLifeMalus;
+    private int currentLifeMalus;
+    private int difficultyValueLifeMalus;
+
+    public int maxAttackMalus;
+    private int currentAttackMalus;
+    private int difficultyValueAttackMalus;
+
+    public int maxSpeedMalus;
+    private int currentSpeedMalus;
+    private int difficultyValueSpeedMalus;
+
+    public int maxSteleMalus;
+    private int currentSteleMalus;
+    private int difficultyValueSteleMalus;
+
+    #endregion
+
+    void Start()
+    {
+        //Init
+        currentLifeMalus = 0;
+        currentAttackMalus = 0;
+        currentSpeedMalus = 0;
+        currentSteleMalus = 0;
+
+        //Init text
+        DifficultyTextUpdate(MalusType.All);
+    }
+
+    void Update()
+    {
+        
+    }
+
+    #region Methods
+
+    private void DifficultyTextUpdate(MalusType malus)
+    {
+        switch (malus)
+        {
+            case MalusType.Life:
+                currentLifeMalusText.text = "+ " + (currentLifeMalus * 20).ToString() + "%";
+                break;
+            case MalusType.Attack:
+                currentAttackMalusText.text = "+ " + (currentAttackMalus * 25).ToString() + "%";
+                break;
+            case MalusType.Speed:
+                currentSpeedMalusText.text = "+ " + (currentSpeedMalus * 10).ToString() + "%";
+                break;
+            case MalusType.Stele:
+                currentSteleMalusText.text = "- " + (currentSteleMalus * 2).ToString();
+                break;
+            case MalusType.All:
+                currentLifeMalusText.text = "+ " + (currentLifeMalus * 20).ToString() + "%";
+                currentAttackMalusText.text = "+ " + (currentAttackMalus * 25).ToString() + "%";
+                currentSpeedMalusText.text = "+ " + (currentSpeedMalus * 10).ToString() + "%";
+                currentSteleMalusText.text = "- " + (currentSteleMalus * 2).ToString();
+                break;
+            default:
+                break;
+        }
+    }
+    //Update the text of the corresponding malus
+
+    private void DifficultyButtonsUpdate(int value, int maxValue, Button minusButton, Button plusButton)
+    {
+        if (value == 0) minusButton.interactable = false;
+        else if (value == maxValue) plusButton.interactable = false;
+        else
+        {
+            minusButton.interactable = true;
+            plusButton.interactable = true;
+        }
+    }
+    //Makes the minus/plus button (difficulty) non-interactable/interactable depending of the new malus value
+
+    #endregion
+
+    #region Buttons methods
+
+    public void ChangeOpenedTab()
+    {
+        nextOpenedTab = null;
+
+        if (currentOpenedTab != nextOpenedTab)
+        {
+            if (currentOpenedTab == null)
+            {
+                //Open the next tab
+                currentOpenedTab = nextOpenedTab;
+                return;
+            }
+
+            //Close the current tab
+            //Then open the next tab
+            currentOpenedTab = nextOpenedTab;
+        }
+        else
+        {
+            //Close the current tab
+            currentOpenedTab = null;
+        }
+
+        nextOpenedTab = null;
+    }
+
+    public void ChangeDifficultyButton(DifficultyButton button)
+    {
+        int sign = 0;
+        if (button.plus) sign = 1;
+        else sign = -1;
+
+        switch (button.malus)
+        {
+            case MalusType.Life:
+                currentLifeMalus += sign;
+                totalDifficulty += difficultyValueLifeMalus;
+                DifficultyTextUpdate(MalusType.Life);
+                DifficultyButtonsUpdate(currentLifeMalus, maxLifeMalus, minusLifeButton, plusLifeButton);
+                break;
+            case MalusType.Attack:
+                currentAttackMalus += sign;
+                totalDifficulty += difficultyValueAttackMalus;
+                DifficultyTextUpdate(MalusType.Attack);
+                DifficultyButtonsUpdate(currentAttackMalus, maxAttackMalus, minusAttackButton, plusAttackButton);
+                break;
+            case MalusType.Speed:
+                currentSpeedMalus += sign;
+                totalDifficulty += difficultyValueSpeedMalus;
+                DifficultyTextUpdate(MalusType.Speed);
+                DifficultyButtonsUpdate(currentSpeedMalus, maxSpeedMalus, minusSpeedButton, plusSpeedButton);
+                break;
+            case MalusType.Stele:
+                currentSteleMalus += sign;
+                totalDifficulty += difficultyValueSteleMalus;
+                DifficultyTextUpdate(MalusType.Stele);
+                DifficultyButtonsUpdate(currentSteleMalus, maxSteleMalus, minusSteleButton, plusSteleButton);
+                break;
+            case MalusType.All:
+                break;
+            default:
+                break;
+        }
+    }
+    //When a malus button is clicked, check which one (minus/plus), and remove/add an unit to the corresponding malus type.
+    //Also, makes the button non-interactable if it's out of range (0 to maxValue).
+
+    #endregion
+
+}

83a5a9464cd5cad600a11f66f66ab6ec87c94b7d
Destati <joanna.ehrhardt@e-artsup.net>
2021-06-01 09:01:12 +0100

    Images documents sans textes intégrer dans le projet.

---
+++ Assets/JoannaTest/Loading.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Loading : MonoBehaviour
+{
+
+    public GameObject LoadingScreen1;
+    public GameObject LoadingScreen2;
+    public GameObject LoadingScreen3;
+    public GameObject LoadingScreen4;
+    public GameObject LoadingScreen5;
+    public GameObject LoadingScreen6;
+    public GameObject LoadingScreen7;
+    public GameObject LoadingScreen8;
+    public GameObject LoadingScreen9;
+    int index;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       // index = Random.Range(0, GameObject, );
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

3af3a24c7ebba4661645ebdb618a14ee0847726e
GERARD_Maxime <maxime.gerard@e-artsup.net>
2021-06-08 11:59:48 +0200

    New Guardian effect

--- Assets/Project/Code/Scripts/SteleEffects/GuardianAmelioration.cs
+++ Assets/Project/Code/Scripts/SteleEffects/GuardianAmelioration.cs
@@ -15,16 +15,22 @@ public class GuardianAmelioration : SteleAmelioration
                 guardianStats.GetStat(StatType.PhysicalResistances).AddModifier(new StatModifier(0.1f, StatType.PhysicalResistances, StatModType.PercentAdd, this));
                 guardianStats.GetStat(StatType.MagicalResistances).AddModifier(new StatModifier(0.1f, StatType.MagicalResistances, StatModType.PercentAdd, this));
 
+                MyAnimator.SetInteger("Evolution", 1);
+
                 Debug.Log("UPGRADE 1");
                 break;
             case SteleLevel.EvolutionII: // 20% puissance, 20% as
                 guardianStats.GetStat(StatType.PhysicalResistances).AddModifier(new StatModifier(0.2f, StatType.PhysicalResistances, StatModType.PercentAdd, this));
                 guardianStats.GetStat(StatType.MagicalResistances).AddModifier(new StatModifier(0.2f, StatType.MagicalResistances, StatModType.PercentAdd, this));
 
+                MyAnimator.SetInteger("Evolution", 2);
+
                 Debug.Log("UPGRADE 2");
                 break;
             case SteleLevel.FinalEvolution: // Stun zone added
                 if (!stunZone.activeInHierarchy) stunZone.SetActive(true);
+
+                MyAnimator.SetInteger("Evolution", 3);
 
                 Debug.Log("UPGRADE 3");
                 break;

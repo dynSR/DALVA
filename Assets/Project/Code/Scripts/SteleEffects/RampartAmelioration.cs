e49628f5398fc2b33560917aa26c3ea51e29c376
GERARD_Maxime <maxime.gerard@e-artsup.net>
2021-06-08 11:07:32 +0200

    Reduced the ice ring of frost effect, inferno effect updated

--- Assets/Project/Code/Scripts/SteleEffects/RampartAmelioration.cs
+++ Assets/Project/Code/Scripts/SteleEffects/RampartAmelioration.cs
@@ -12,16 +12,22 @@ public class RampartAmelioration : SteleAmelioration
             case SteleLevel.EvolutionI: // reductions +10%
                 rampartDamageZone.DamagePerSecond *= 1.5f;
 
+                MyAnimator.SetInteger("Evolution", 1);
+
                 Debug.Log("UPGRADE 1");
                 break;
             case SteleLevel.EvolutionII: // reductions +7.5% + 1 range
                 rampartDamageZone.DamagePerSecond *= 2f;
+
+                MyAnimator.SetInteger("Evolution", 2);
 
                 Debug.Log("UPGRADE 2");
                 break;
             case SteleLevel.FinalEvolution: // rotating projectiles / 20% réduction délai d'application des dégâts
                 rampartDamageZone.rotatingProjectiles.SetActive(true);
                 rampartDamageZone.Interval = 0.35f;
+
+                MyAnimator.SetInteger("Evolution", 3);
 
                 Debug.Log("UPGRADE 3");
                 break;

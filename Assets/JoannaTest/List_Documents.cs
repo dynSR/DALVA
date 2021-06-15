61066513c6fcb656108f6e10da7a821b2d20afc6
Destati <joanna.ehrhardt@e-artsup.net>
2021-06-15 13:20:03 +0100

    MainMenu documents buttons functional.

---
+++ Assets/JoannaTest/List_Documents.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class List_Documents : MonoBehaviour
+{
+
+    public GameObject Document1;
+    public GameObject Document2;
+    public GameObject Document3;
+    public GameObject Document4;
+    public GameObject Document5;
+    public GameObject Document6;
+    public GameObject Document7
+        ;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

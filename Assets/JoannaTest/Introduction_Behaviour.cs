cc110148a3cb6284b483b6dfd5163f87f7a2e965
Destati <joanna.ehrhardt@e-artsup.net>
2021-06-15 14:56:00 +0100

    .

--- Assets/JoannaTest/Introduction_Behaviour.cs
+++ Assets/JoannaTest/Introduction_Behaviour.cs
@@ -7,7 +7,7 @@ using UnityEngine.UI;
 public class Introduction_Behaviour : MonoBehaviour
 {
 
-    public GameObject blackOutSquare;
+
 
 
     // Start is called before the first frame update
@@ -22,42 +22,13 @@ public class Introduction_Behaviour : MonoBehaviour
         // Detects if any key has been pressed.
         if (Input.anyKey)
         {
-            StartCoroutine(FadeBlackOutSquare());
+
+            SceneManager.LoadScene("Scene_MainMenu");
             Debug.Log("A key or mouse click has been detected");
 
         }
 
     }
 
-   public IEnumerator FadeBlackOutSquare(bool fadeToBlack = true, int fadeSpeed =5)
-    {
-        Color objectColor = blackOutSquare.GetComponent<Image>().color;
-        float fadeAmout;
-
-        if (fadeToBlack)
-        {
-            while (blackOutSquare.GetComponent<Image>().color.a <1)
-            {
-                fadeAmout = objectColor.a + (fadeSpeed * Time.deltaTime);
-
-                objectColor = new Color(objectColor.r, objectColor.g, objectColor.b, fadeAmout);
-                blackOutSquare.GetComponent<Image>().color = objectColor;
-                yield return null;
-                SceneManager.LoadScene("Scene_MainMenu");
-            }
-        }
-
-        else
-        {
-            while (blackOutSquare.GetComponent<Image>().color.a> 0)
-            {
-                fadeAmout = objectColor.a - (fadeSpeed * Time.deltaTime);
-
-                objectColor = new Color(objectColor.r, objectColor.g, objectColor.b, fadeAmout);
-                blackOutSquare.GetComponent<Image>().color = objectColor;
-                yield return null;
-
-            }
-        }
-    }
+ 
 }
---
+++ Assets/JoannaTest/Loading_Introduction_MainMenu.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Loading_Introduction_MainMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        SceneManager.LoadScene("Scene_Narrative_Introduction");
+    }
+}
